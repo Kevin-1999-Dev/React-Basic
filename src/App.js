78239f6e1b333a7c785392c4cd62a3e8d5901078
Kaung Zaw Han <kaungzawhan.zotefamily@gmail.com>
2025-08-24 22:40:08 +0630

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let name = "Kevin";
+  let changeNameButton = () => {
+    name = "Kaung Zaw Han";
+  }
   return (
     <div className="App">
-       Hello
+      <h1>{name}</h1>
+      <button onClick={changeNameButton}>change name</button>
     </div>
   );
 }
