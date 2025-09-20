2c874d1cf8c08ab0fd3b08e835dc01a9935847b1
Kaung Zaw Han <kaungzawhan.zotefamily@gmail.com>
2025-09-20 17:38:09 +0630

    loop posts and delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let name = "Kevin";
-  let changeNameButton = () => {
-    name = "Kaung Zaw Han";
+  
+  let [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'First Post',
+    },
+    {
+      id: 2,
+      title: 'Second Post',
+    },
+    {
+      id: 3,
+      title: 'Third Post',
+    },
+  ]);
+
+  let deletePost = (id) => {
+    setPosts((prevState) => prevState.filter((post) => post.id !== id));
   }
   return (
     <div className="App">
-      <h1>{name}</h1>
-      <button onClick={changeNameButton}>change name</button>
+      <h1>Posts</h1>
+      <ul>
+        {!!posts.length && posts.map((post)=>(
+          <li key={post.id}>{post.title} <button onClick={()=>deletePost(post.id)}>delete</button></li>
+        ))}
+        {!posts.length && <p>No posts available</p>}
+      </ul>
     </div>
   );
 }
